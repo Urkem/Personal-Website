c7e4ac05c8898e3f47f9579479a14d6b4752bb6d
Uros Milovanovic <urkem98@gmail.com>
2020-09-13 21:02:18 +0200

    inital commit

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+function Navbar() {
+    return (
+        <div>
+            <p>pls work</p>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar/>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
