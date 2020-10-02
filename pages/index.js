5933e51071462f1e51fcf86b587951ecf05a34bc
Uros Milovanovic <urkem98@gmail.com>
2020-10-02 23:30:45 +0200

    home page done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
 
+import styles from '../styles/Home.module.css'
+
 export default function Home() {
   return (
     <div>
@@ -11,14 +13,34 @@ export default function Home() {
 
       <Navbar/>
 
-      <footer>
-        <a
-          href="/Uros_Milovanovic_CV.pdf"
-          target="_blank"
-        >
-          <img src="/vercel.svg" alt="Vercel Logo" />
+      <header className={styles.containerHeader}>
+        <img src="/Profile_pic.png" className={styles.profile}/>
+        <a href="">
+          <h1 className={styles.name}>Uroš Milovanović</h1>
         </a>
-      </footer>
+        <a>
+          <h2>Aspiring Software Dev</h2>
+        </a>
+      </header>
+
+      <main className="containerMain">
+        <div className="about">
+          <h3>
+            About Me
+          </h3>
+          <p>
+            My primary interest lies in doing computer related projects
+            form tinkering with linux on a raspberry pi to solving problems on LeetCode.com. 
+            I’m also an avid community member of many technology subreddits (r/buildapc, r/dailyprogrammer, r/selfhosted...).
+          </p>
+        </div>
+        <div className="contact">
+            My primary interest lies in doing computer related projects
+            form tinkering with linux on a raspberry pi to solving problems on LeetCode.com. 
+            I’m also an avid community member of many technology subreddits (r/buildapc, r/dailyprogrammer, r/selfhosted...).
+        </div>
+      </main>
+
     </div>
   )
 }
